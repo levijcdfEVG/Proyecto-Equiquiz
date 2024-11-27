618c2b851fd32a82617be31f72570681071d8e18
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-11-27 12:56:57 +0000

    Se ha empezado a programar la parte de interaccion con botones

---
+++ assets/js/activarJugar.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function activarBoton() {
+    let botonJugar = document.getElementById('contendorJugar');
+    if(botonJugar.disabled)
+        botonJugar.disabled = false;
+}
+
---
+++ assets/js/gestionarMenus.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+function gestionarSecciones(menuPart){
+    //Elementos Globales
+    let botonHistoria = document.getElementById('botonHistoria');
+    let botonRanking = document.getElementById('botonRanking');
+    let main = document.getElementById('contenedorBotones');
+    switch (menuPart) {
+        case 'historia':
+            
+            
+            break;
+        case 'ranking':
+            
+            break;
+
+        default:
+            break;
+    }
+}
