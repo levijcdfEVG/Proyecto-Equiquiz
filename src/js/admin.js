5417e4a4f0093ad881c71864547a8412873a38de
zmartinl <zmartinllera1@gmail.com>
2024-12-01 03:16:58 +0100

    modificacion

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,31 +1,6 @@
 window.addEventListener('load', function() {
-    const añadirPersonaje = document.getElementById('añadirPersonajes');
-    const eliminarPersonaje = document.getElementById('eliminarPersonajes');
-    const modificarPersonaje = document.getElementById('modificarPersonajes');
-    const listarPersonaje = document.getElementById('listarPersonajes');
-    const recuperarPersonaje = document.getElementById('recuperarPersonajes');
     const luis = document.getElementById('luis');
     const martina = document.getElementById('martina');
-
-    añadirPersonaje.addEventListener('click', function() {
-        window.location.href = '../../diseño/GestionPersonajes/añadirPersonaje.html';
-    });
-
-    eliminarPersonaje.addEventListener('click', function() {
-        window.location.href = '../../diseño/GestionPersonajes/eliminarPersonajes.html';
-    });
-
-    modificarPersonaje.addEventListener('click', function() {
-        window.location.href = '../../diseño/GestionPersonajes/modificarPersonajes.html';
-    });
-
-    listarPersonaje.addEventListener('click', function() {
-        window.location.href = '../../diseño/GestionPersonajes/listarPersonajes.html';
-    });
-
-    recuperarPersonaje.addEventListener('click', function() {
-        window.location.href = '../../diseño/GestionPersonajes/recuperarPersonaje.html';
-    });
 
     luis.addEventListener('click', function() {
         window.location.href = '../../diseño/GestionPersonajes/modificarParte2.html';
