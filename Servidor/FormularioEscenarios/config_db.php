aa143611c26e339734f934142e1306a610863bd9
maria <mariavidigalbarroso.guadalupe@alumnado.fundacionloyola.net>
2024-12-02 13:11:23 +0100

    Formulario

---
+++ Servidor/FormularioEscenarios/config_db.php
@@ -0,0 +1,13 @@
+<?php
+    /*Local*/
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $contraseña = '';
+    $basedatos = 'equiquiz';
+    
+    /*Servidor*/
+    $servidor = 'esvirgua.com';
+    $usuario = 'user2daw_21';
+    $contraseña = 'rU}s@+[kCz)y';
+    $basedatos = 'user2daw_BD1-21';
+?>
