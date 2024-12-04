b1532898a623ea32039fcc3dc019e04df6b2c468
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-12-04 09:30:22 +0000

    Se ha validado los campos de respuestas de formulario

--- src/app/admin/view/js/validarInsercionPregunta.js
+++ src/app/admin/view/js/validarInsercionPregunta.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let botonSubir = document.getElementById('submitButton');
 
 
-    // Añadir respuestas
+    // Añadir y quitar respuestas
     let aniadirRespuestas = document.getElementById('aniadirRespuestas');
     let contenedorDeRespuestas = document.getElementById('contenedorDeRespuestas');
+    let removeRespuesta = document.getElementById('quitarRespuestas');
 
     let counter = 1;
 
+    //Añadir respuestas
     aniadirRespuestas.addEventListener('click', (event) => {
         event.preventDefault(); //Bloquea la subida del form cuando se clica en el boton
 
@@ -23,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
             let newInput = document.createElement('input');
             newInput.type = 'text';
             newInput.name = 'respuestas';
-            newInput.id = `respuestas-${counter}`;
+            newInput.id = 'respuestas'+counter;
             newInput.required = true;
 
             // Se añade el nuevo campo al contenedor
@@ -31,6 +33,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //Quitar respuestas
+    removeRespuesta.addEventListener('click', (event) => {
+        event.preventDefault(); //Bloquea la subida del form cuando se clica en el boton
+        
+        if (counter <= 1) {
+            alert("No se pueden quitar más preguntas");
+        } else {
+            let ultimaRespuesta = document.getElementById('respuestas'+counter);
+            ultimaRespuesta.remove();
+            counter--;
+        }
+    });
 
     //Validacion de longitud de values de formulario
     //Pregunta
@@ -44,6 +58,25 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             console.log("Longitud OK");
             botonSubir.removeAttribute('disabled'); // Habilitar el botón si la longitud es válida
+        }
+        
+    });
+
+    //Respuestas
+    let respuestas = document.getElementById('respuestas'+counter);
+    respuestas.addEventListener('focusout', ()=>{
+        let longitud = respuestas.value.length;
+
+        if (longitud > 300) {
+            alert("La longitud de la respuesta supera el límite establecido (300 caracteres)");
+            botonSubir.setAttribute('disabled', 'true'); // Deshabilitar el botón
+            aniadirRespuestas.setAttribute('disabled', 'true'); //Deshabilitar la opcion de añadir una pregunta más
+            removeRespuesta.setAttribute('disabled', 'true'); //Deshabilitar la opcion de añadir una pregunta más
+        } else {
+            console.log("Longitud OK");
+            botonSubir.removeAttribute('disabled'); // Habilitar el botón si la longitud es válida
+            aniadirRespuestas.removeAttribute('disabled');
+            removeRespuesta.removeAttribute('disabled');
         }
         
     });
