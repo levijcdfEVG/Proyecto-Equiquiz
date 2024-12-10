1a90fc5d6f7ada3dc7ceb3e86f1539f520149f49
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-12-10 13:07:59 +0100

    Se ha documentado los metodos ya implementados

--- src/app/admin/controller/CPreguntas.php
+++ src/app/admin/controller/CPreguntas.php
@@ -1,46 +1,82 @@
 <?php
 
-    class CPreguntas {
+/**
+ * Class CPreguntas
+ *
+ * This class handles the operations related to "Preguntas" (questions), including showing different views for modifying,
+ * listing, deleting, and adding questions. It utilizes the MPersonaje model for data operations.
+ */
+class CPreguntas {
 
-        private $MPreguntas;
-        public $view;
-        public $title;
+    /**
+     * @var MPersonaje $MPreguntas Instance of the MPersonaje model.
+     */
+    private $MPreguntas;
 
-        /**
-         * Summary of __construct
-         * 
-         * En el constructor se inicializa el modelo Personaje.
-         */
-        public function __construct() {
-            require_once '../config/config.php';
-            // Requerimos de la clase del modelo.
-            require_once 'model/MPersonaje.php';
-            $this->MPersonaje = new MPersonaje();
-        }
+    /**
+     * @var string $view The view to be rendered.
+     */
+    public $view;
 
+    /**
+     * @var string $title The title of the current view.
+     */
+    public $title;
 
-        //Visualizar 
-        public function showModify() {
-            $this->title = 'Modificacion de Preguntas';
-            $this->view = 'modificarPreguntas.php';
-        }
-        //ViewList
-        public function showList() {
-            $this->title = 'Listado de Preguntas';
-            $this->view = 'listarPreguntas.php';
-        }
-        //ViewDelete
-        public function showDelete() {
-            $this->title = 'Borrado de Preguntas';
-            $this->view = 'borrarPreguntas.php';
-        }
-        //ViewAdd
-        public function showAdd() {
-            $this->title = 'Añadir Preguntas';
-            $this->view = 'aniadirPreguntas.php';
-        }
-        //GetQuestionData
-
-        //GetAnswerData
-
+    /**
+     * CPreguntas constructor.
+     *
+     * In the constructor, the MPersonaje model is initialized.
+     */
+    public function __construct() {
+        require_once '../config/config.php';
+        // Requiring the model class.
+        require_once 'model/MPersonaje.php';
+        $this->MPersonaje = new MPersonaje();
     }
+
+    /**
+     * Show the view for modifying questions.
+     */
+    public function showModify() {
+        $this->title = 'Modificacion de Preguntas';
+        $this->view = 'modificarPreguntas.php';
+    }
+
+    /**
+     * Show the view for listing questions.
+     */
+    public function showList() {
+        $this->title = 'Listado de Preguntas';
+        $this->view = 'listarPreguntas.php';
+    }
+
+    /**
+     * Show the view for deleting questions.
+     */
+    public function showDelete() {
+        $this->title = 'Borrado de Preguntas';
+        $this->view = 'borrarPreguntas.php';
+    }
+
+    /**
+     * Show the view for adding questions.
+     */
+    public function showAdd() {
+        $this->title = 'Añadir Preguntas';
+        $this->view = 'aniadirPreguntas.php';
+    }
+
+    // Future methods for handling question data operations.
+
+    // GetQuestionData
+
+    // GetAnswerData
+
+    // ModifyQuestions
+
+    // DeleteQuestions
+
+    // ShowQuestions
+}
+?>
