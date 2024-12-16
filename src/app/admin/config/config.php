d7d760e5f95ff0e66c761e93b9d2903213173a2e
zmartinl <zmartinllera@gmail.com>
2024-12-16 22:12:22 +0100

    correcto

---
+++ src/app/admin/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    define("RUTA", "../");
+    define("VIEWPATHADMIN", RUTA . "admin/view/");
+    define("PATHIMGADMIN", "assets/img/");
+    define("RUTACONTROLLERADMIN", "controller/");
+    define("DEFAULTCONTROLLER", "CAdministrador");
+    define("DEFAULTVIEW", "view");
+    define("HEADADMIN", VIEWPATHADMIN . "head.php");
+    define("FOOTERADMIN", VIEWPATHADMIN . "footer.php");
+?>
---
+++ src/app/user/config/configDb.php
@@ -0,0 +1,5 @@
+<?php
+    define("DSN",'mysql:host=esvirgua.com;dbname=user2daw_BD1-21;charset=utf8mb4');
+    define("USUARIO",'user2daw_21');
+    define("PASSWORD",'rU}s@+[kCz)y');
+?>
